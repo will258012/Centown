9c067b8ca63e15356bfbdae64627c0a8d3487b1d
will258012 <125041335+will258012@users.noreply.github.com>
2024-02-12 12:58:03 +0800

    Update Apply-Image.cmd

--- Apply-Image.cmd
+++ Apply-Image.cmd
@@ -1,28 +1,39 @@
 @echo off
+setlocal
 echo Please Wait...
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    
-    echo You need to run this script as an administrator.
-    echo Please right-click on the script and select "Run as administrator".
-    pause
-    exit /b
+color 4
+echo =========================================================
+echo You need to run this script as an administrator.
+echo Please right-click on the script and select 'Run as administrator'.
+pause
+exit /b
 )
-setlocal
+
 
 set "BaseDir=%~dp0"
-mkdir %BaseDir%World
+mkdir "%BaseDir%World" >nul
 set "ImageFile=%BaseDir%Image\Centown.swm"
 set "SWMFile=%BaseDir%Image\Centown*.swm"
 set "ApplyDir=%BaseDir%World"
 Dism /Apply-Image /ImageFile:"%ImageFile%" /SWMFile:"%SWMFile%" /Index:1 /ApplyDir:"%ApplyDir%" /CheckIntegrity /Verify
-if '%errorlevel%' == '0' (
-    
-
-
+if '%errorlevel%' == '2' (
+color 4
+echo =========================================================
+echo Do you forget to download the image files?
+echo Please open the link in "Image/README.md" to download.
+) else if '%errorlevel%' NEQ '0' (
+color 4
+echo =========================================================
+echo Something went wrong.
+echo Please check your image files.
+) else (
+color 2
+cls
+echo =========================================================
+echo Success!
+echo Centown save shall be in the 'World' folder.
 )
-else (
-
-)
-    pause
-    exit /b
+pause
+exit /b
